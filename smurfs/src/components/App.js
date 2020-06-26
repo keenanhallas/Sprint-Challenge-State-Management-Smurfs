dde45bfefe2da91ef3d769d2796d3a813529a794
Keenan <keenanhallas@gmail.com>
2020-06-26 19:48:26 -0400

    smurf data coming into the list component via connect

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import { SmurfForm } from "./SmurfForm";
-import { SmurfList } from "./SmurfList";
+import SmurfList from "./SmurfList";
 
 class App extends Component {
   render() {
--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getSmurfs } from "../actions";
 
-export const SmurfList = () => {
+const SmurfList = ({smurfs, getSmurfs}) => {
+    useEffect(() => {
+        getSmurfs();
+    }, [smurfs]);
+
+    console.log(smurfs);
     return (
         <h2>This will be a list</h2>
     );
 }
+
+const mapStateToProps = state => {
+    return {
+        smurfs: state.smurfs
+    }
+}
+
+export default connect(mapStateToProps, {getSmurfs})(SmurfList);
