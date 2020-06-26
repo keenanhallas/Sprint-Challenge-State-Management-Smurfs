9c8dab77363c101c7eedcc4f9f787350763e50c7
Keenan <keenanhallas@gmail.com>
2020-06-26 19:23:00 -0400

    shell of getSmurfs action written

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,15 +1,21 @@
 const initialState = {
     smurfs: []
+    isFetching: false,
+    error: ""
   }
   
 export const smurfsReducer = (state = initialState, action) => {
     switch (action.type) {
         case "FETCHING_SMURFS_START":
-            //code here
+            return {
+                ...state,
+                isFetching: true,
+                error: ""
+            }
         case "FETCHING_SMURFS_SUCCESS":
-            //code here
+            //map over data here and set smurfs
         case "FETCHING_SMURFS_FAILURE":
-            //code here
+            //set error here
         case "ADD_SMURF":
             //code here
         default:
